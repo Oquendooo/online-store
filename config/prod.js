c611dd9fb2745ff67bcac30c810827fdf78e7ffb
Aaron <iaaron94@gmail.com>
2018-11-18 12:06:24 -0500

    changed prod keys

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,18 @@
 //dev.js - don't commit this. These are the development keys
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
+
 module.exports = {
   googleClientID:
     '137009873334-kabl911n49nom6at728bkamukrhojk6f.apps.googleusercontent.com',
   googleClientSecret: 'cKgBLglGEkFWVJfMkvLHfCZE',
-  mongoURI: 'mongodb://aarondev:password94@ds211694.mlab.com:11694/ecommerce-store-dev',
+  mongoURI: 'mongodb://aaronprod:password94@ds211694.mlab.com:11694/ecommerce-store-dev',
   cookieKey: 'kggkjskgskdkshjuenchd'
 };
