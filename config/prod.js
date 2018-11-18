9086d0b618d5b0301c20599d699fcd1f4ec62ac0
Aaron <iaaron94@gmail.com>
2018-11-18 11:38:52 -0500

    inital commit

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//dev.js - don't commit this. These are the development keys
+module.exports = {
+  googleClientID:
+    '137009873334-kabl911n49nom6at728bkamukrhojk6f.apps.googleusercontent.com',
+  googleClientSecret: 'cKgBLglGEkFWVJfMkvLHfCZE',
+  mongoURI: 'mongodb://aarondev:password94@ds211694.mlab.com:11694/ecommerce-store-dev',
+  cookieKey: 'kggkjskgskdkshjuenchd'
+};
