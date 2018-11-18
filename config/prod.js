eb435b6684af7d68a7c6b9b16b51d98c489c34f1
Aaron <iaaron94@gmail.com>
2018-11-18 15:05:43 -0500

    change prod keys file

--- config/prod.js
+++ config/prod.js
@@ -6,13 +6,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-
-
-module.exports = {
-  googleClientID:
-    '137009873334-kabl911n49nom6at728bkamukrhojk6f.apps.googleusercontent.com',
-  googleClientSecret: 'cKgBLglGEkFWVJfMkvLHfCZE',
-  mongoURI: 'mongodb://aaronprod:password94@ds211694.mlab.com:11694/ecommerce-store-dev',
-  cookieKey: 'kggkjskgskdkshjuenchd'
-};
