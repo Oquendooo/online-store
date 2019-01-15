2c5cceceb11be287419d6f4995193a8057c333b3
Aaron <iaaron94@gmail.com>
2019-01-15 14:31:29 -0500

    adding react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import ProductPage from './components/ProductPage/ProductPage';
 import Products from './components/DisplayProductsPage/Products';
 import Header from './components/Header';
 import Banner from './components/Banner';
+import LandingPage from './components/LandingPage';
 import Breadcrumbs from './components/Breadcrumbs';
 import DisplayProductsPage from "./components/DisplayProductsPage";
 
@@ -10,10 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Banner />
-        <Breadcrumbs />
-        <DisplayProductsPage />
+          <Header />
+          <Banner />
+          <Route exact path='/' component={LandingPage}/>
+          <Route exact path='/products' component={Breadcrumbs} />
+          <Route exact path='/products' component={DisplayProductsPage}/>
+          <Route exact path='/product' component={ProductPage}/>
       </div>
     );
   }
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,284 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            <div id="main-content" className="">
+
+                <div className="row1-wrapper">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-12 col-lg-6">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row1img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-12 col-lg-6">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row1img2.jpg" alt=""/>
+                                </a>
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row1img3.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row2-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <img className="img-fluid" src="/assets/images/row2im1.jpg" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row3-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <div className="container brand-slider-wrapper">
+                                    <div className="brand-slider">
+                                        <div className="owl-carousel owl-theme">
+                                            <div className="item brand" style={{width:'auto'}}>
+                                                <a href="pages/display-items.html">
+                                                    <img className="img-fluid" src="/assets/images/row3img1.jpg" alt=""/>
+                                                </a>
+                                            </div>
+                                            <div className="item brand" style={{width:'auto'}}>
+                                                <a href="pages/display-items.html">
+                                                    <img className="img-fluid" src="/assets/images/row3img2.jpg" alt=""/>
+                                                </a>
+                                            </div>
+                                            <div className="item brand" style={{width:'auto'}}>
+                                                <a href="pages/display-items.html">
+                                                    <img className="img-fluid" src="/assets/images/row3img3.jpg" alt=""/>
+                                                </a>
+                                            </div>
+                                            <div className="item brand" style={{width:'auto'}}>
+                                                <a href="pages/display-items.html">
+                                                    <img className="img-fluid" src="/assets/images/row3img4.jpg" alt=""/>
+                                                </a>
+                                            </div>
+                                            <div className="item brand" style={{width:'auto'}}>
+                                                <a href="pages/display-items.html">
+                                                    <img className="img-fluid" src="/assets/images/row3img5.jpg" alt=""/>
+                                                </a>
+                                            </div>
+                                            <div className="item brand" style={{width:'auto'}}>
+                                                <a href="pages/display-items.html">
+                                                    <img className="img-fluid" src="/assets/images/row3img6.jpg" alt=""/>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row5-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid"
+                                         src="https://cdn.karmaloopassets.com/media/gene-cms/s/h/shopourfeed_1__1240x100.jpg"
+                                         alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row6-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="container brand-slider-wrapper">
+                                <div className="brand-slider">
+                                    <div className="owl-carousel owl-theme">
+                                        <div className="col-lg-4 item brand" style={{width:'auto'}}>
+                                            <a href="pages/display-items.html">
+                                                <img className="img-fluid" src="/assets/images/row6img1.jpg" alt=""/>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-4 item brand" style={{width:'auto'}}>
+                                            <a href="pages/display-items.html">
+                                                <img className="img-fluid" src="/assets/images/row6img2.jpg" alt=""/>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-4 item brand" style={{width:'auto'}}>
+                                            <a href="pages/display-items.html">
+                                                <img className="img-fluid" src="/assets/images/row6img3.jpg" alt=""/>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-4 item brand" style={{width:'auto'}}>
+                                            <a href="pages/display-items.html">
+                                                <img className="img-fluid" src="/assets/images/row6img4.jpg" alt=""/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row7-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row7img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row8-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row8img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row9-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12 col-lg-6">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid margin-add" src="/assets/images/row9img1.jpg" alt=""/>
+                                </a>
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid margin-add" src="/assets/images/row6img2.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-12 col-lg-6">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row6img3.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row10-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12 col-md-6 col-lg-6">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row10img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-12 col-md-6 col-lg-6">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row10img2.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row11-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-6 col-md-4 col-lg-4">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row11img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-6 col-md-4 col-lg-4">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row6img2.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-6 col-md-4 col-lg-4">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row6img3.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row12-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid"
+                                         src="/assets/images/row12img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row13-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-6 col-md-4 col-lg-4">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row13img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-6 col-md-4 col-lg-4">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row13img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div className="col-xs-6 col-md-4 col-lg-4">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid" src="/assets/images/row13img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row14-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <a href="pages/display-items.html">
+                                    <img className="img-fluid"
+                                         src="/assets/images/row14img1.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row15-wrapper">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xs-12">
+                                <p>Karmaloop was created to bring you the latest in Streetwear trends from Top Streetwear Brands
+                                    Like Billionaire Boys Club, 10 Deep, Pink Dolphin, Kappa, Adidas, and more. Karmaloop remains
+                                    the largest Store for Urban Clothing Since it opened
+                                    its doors in 2000. This includes Kazbah Marketplace which bring you upcoming brands and styles
+                                    you can’t find anywhere else and PLNDR The largest Discount for Streetwear. From the latest
+                                    Shoes, Joggers, Outerwear, T-shirts Karmaloop has
+                                    what your looking for in Women’s and Men’s Fashion.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default LandingPage;
