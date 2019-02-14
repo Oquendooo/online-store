b108c1aad1dd413d6091c6828d71cd21a5358331
Aaron <iaaron94@gmail.com>
2019-02-14 18:38:04 -0500

    testing

--- services/passport.js
+++ services/passport.js
@@ -63,4 +63,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
 
 // Tell passport to use this strategy
 // passport.use(jwtLogin);
-passport.use(localLogin);
+//passport.use(localLogin);
